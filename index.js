5213e776eefa56e50df83c7f47372652a4d177aa
Zach Dubno <zachedubno@gmail.com>
2017-07-10 15:24:46 -0400

    finished lab

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
 function findMinAndRemove(array){
-
+  let min = array[0]
+  let mindex = 0
+  for (let i = 0;i<array.length;i++){
+    if (array[i] < min){
+      min = array[i]
+      mindex = i
+    }
+  }
+  array.splice(mindex, 1)
+  return min
 }
 
 function insertionSort(array){
-
+  let sort = []
+  while(array.length > 0){
+    sort.push(findMinAndRemove(array))
+  }
+  return sort
 }
